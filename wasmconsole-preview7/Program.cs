61e840c203fd3c818e1c877f8fb9c25b0a3def98
yamachu <yamachu.dev@gmail.com>
2022-07-31 04:13:45 +0900

    JSExportを使ったやり方でPreview7のtemplateを試す

--- wasmconsole-preview7/Program.cs
+++ wasmconsole-preview7/Program.cs
@@ -1,3 +1,19 @@
 using System;
+using System.Runtime.InteropServices.JavaScript;
 
 Console.WriteLine("Hello, Console!");
+
+public static partial class SampleClass
+{
+    [JSExport]
+    internal static string CallFromJS()
+    {
+        return "Hello, World!";
+    }
+
+    [JSExport]
+    internal static int Add42(int number)
+    {
+        return number + 42;
+    }
+}
--- wasmconsole-preview7/main.mjs
+++ wasmconsole-preview7/main.mjs
@@ -1,5 +1,11 @@
-import { App } from './app-support.mjs'
+import { App } from "./app-support.mjs";
 
 App.main = async function (applicationArguments) {
+    const exports = await App.MONO.mono_wasm_get_assembly_exports("wasmconsole.dll");
+    const callFromJS = exports.SampleClass.CallFromJS;
+    console.log(callFromJS());
+    const add42 = exports.SampleClass.Add42;
+    console.log(add42(100));
+
     await App.MONO.mono_run_main("wasmconsole.dll", applicationArguments);
-}
+};
