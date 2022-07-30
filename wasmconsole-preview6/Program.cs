016834bfc006032a57ae9336302e575a17774c91
yamachu <yamachu.dev@gmail.com>
2022-07-30 14:05:06 +0900

    Add run static method sample

--- wasmconsole-preview6/Program.cs
+++ wasmconsole-preview6/Program.cs
@@ -1,3 +1,16 @@
 using System;
 
 Console.WriteLine("Hello, Console!");
+
+public static partial class SampleClass
+{
+    public static string CallFromJS()
+    {
+        return "Hello, World!";
+    }
+
+    public static int Add42(int number)
+    {
+        return number + 42;
+    }
+}
--- wasmconsole-preview6/main.cjs
+++ wasmconsole-preview6/main.cjs
@@ -1,5 +1,10 @@
 const { App } = require("./app-support.cjs");
 
 App.init = async function () {
+    const callFromJS = App.BINDING.bind_static_method("[wasmconsole] SampleClass:CallFromJS");
+    console.log(callFromJS());
+    const add42 = App.BINDING.bind_static_method("[wasmconsole] SampleClass:Add42");
+    console.log(add42(100));
+
     await App.MONO.mono_run_main_and_exit("wasmconsole.dll", App.processedArguments.applicationArgs);
 };
